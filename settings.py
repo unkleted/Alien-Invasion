69322d77394249a64ac8cf6891229c6a40e4a278
Justin Davis <jdavis@tjctechnology.com>
2019-10-24 14:40:46 -0400

    Resetting the game

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 1.5
-        self.ship_limit = 3
+        self.ship_limit = 2
 
         # Bullet settings
         self.bullet_speed = 1.0
