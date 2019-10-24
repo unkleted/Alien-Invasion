b15f5e7fb30edf8c251bcbd5231dc364102f4e25
Justin Davis <jdavis@tjctechnology.com>
2019-10-24 13:00:55 -0400

    aliens that reach the bottom of the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -150,6 +150,9 @@ class AlienInvasion:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         # Decrement ships_left.
@@ -165,6 +168,15 @@ class AlienInvasion:
 
         # Pause.
         sleep(0.5)
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
 
     def _update_screen(self):
         """Update the images on the screen, and flip to the new screen."""
