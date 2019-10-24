153af3f8f2d221cb14d364fca98eb134e9a6e6c6
Justin Davis <jdavis@tjctechnology.com>
2019-10-24 14:46:14 -0400

    hiding the mouse cursor

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,6 +93,9 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
+
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
         if len(self.bullets) < self.settings.bullets_allowed:
@@ -197,6 +200,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _check_aliens_bottom(self):
         """Check if any aliens have reached the bottom of the screen."""
